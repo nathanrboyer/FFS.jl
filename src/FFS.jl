717322328fb448bc7b3703ff463be86683fc7987
Maxwell Frame <mframe@aiphip.com>
2026-05-27 10:40:19 -0400

    new line description

--- src/FFS.jl
+++ src/FFS.jl
@@ -1,7 +1,8 @@
 """
     Package FFS v$(pkgversion(FFS))
 
-A collection of equations from The American Society of Mechanical Engineers (ASME) Fitness for Service standard FFS-1.
+A collection of equations from The American Society of Mechanical Engineers (ASME)
+Fitness for Service standard FFS-1.
 This package is intended to be used as a dependency of other data processing packages.
 Equations are organized into modules named by their source location in FFS-1.
 """
