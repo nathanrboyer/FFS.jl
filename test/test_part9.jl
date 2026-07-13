ee2c345937d5775bed274324594194646d46f121
Maxwell Frame <mframe@aiphip.com>
2026-07-13 14:35:35 -0400

    formatted pluto KPSCE3 for elliptical plate cracks

--- test/test_part9.jl
+++ test/test_part9.jl
@@ -1,16 +1,16 @@
 @testset "Part 9" begin
 
-    @test Part9.σf(10, 20) == 15
-    @test Part9.max_lpr(1, 2) == 0.5
-    @test Part9.max_kr(0) == 1
-    @test isapprox(Part9.max_kr(0.8), 0.8105591, atol = 1e-4)
+    @test Part9.σ_f(10, 20) == 15
+    @test Part9.Lpr_max(1, 2) == 0.5
+    @test Part9.Kr_max(0) == 1
+    @test isapprox(Part9.Kr_max(0.8), 0.8105591, atol = 1e-4)
     @test Part9.lpr(1, 2) == 0.5
     cvn = 30
-    @test isapprox(Part9.J1mm(cvn), 348.48738, atol = 1e-4)
+    @test isapprox(Part9.J_1mm(cvn), 348.48738, atol = 1e-4)
     jcrit = 243.94116
-    @test isapprox(Part9.Kmat(jcrit, 27.8, 0.3), 86.32652, atol = 1e-4)
-    @test isapprox(Part9.Φ0(1, 2), 0.7071067, atol = 1e-4)
-    @test Part9.Ksrj(2, 3) == 6
+    @test isapprox(Part9.K_mat(jcrit, 27.8, 0.3), 86.32652, atol = 1e-4)
+    @test isapprox(Part9.Φ_0(1, 2), 0.7071067, atol = 1e-4)
+    @test Part9.K_srj(2, 3) == 6
     @test Part9.X(2, 3, 4) == 1.5
 
 end
--- test/test_plate.jl
+++ test/test_plate.jl
@@ -5,22 +5,22 @@
     c = 0.6
     t = 8 # a/t = 0.05
 
-    @test isapprox(KPSCE3.A0(a, c), 0.35931, atol = 1e-4)
-    @test isapprox(KPSCE3.A1(a, c), 0.38601, atol = 1e-4)
-    @test isapprox(KPSCE3.A2(a, c), -0.41703, atol = 1e-4)
-    @test isapprox(KPSCE3.B0(a, c), 1.06929, atol = 1e-4)
-    @test isapprox(KPSCE3.B1(a, c), 0.480883, atol = 1e-4)
-    @test isapprox(KPSCE3.B2(a, c), -0.34824, atol = 1e-4)
-    @test isapprox(KPSCE3.Y0(
+    @test isapprox(KPSCE3.A_0(a, c), 0.35931, atol = 1e-4)
+    @test isapprox(KPSCE3.A_1(a, c), 0.38601, atol = 1e-4)
+    @test isapprox(KPSCE3.A_2(a, c), -0.41703, atol = 1e-4)
+    @test isapprox(KPSCE3.B_0(a, c), 1.06929, atol = 1e-4)
+    @test isapprox(KPSCE3.B_1(a, c), 0.480883, atol = 1e-4)
+    @test isapprox(KPSCE3.B_2(a, c), -0.34824, atol = 1e-4)
+    @test isapprox(KPSCE3.Y_0(
         1.06929, 0.480883, -0.34824, a, t), 1.07049, atol = 1e-4)
-    @test isapprox(KPSCE3.Y1(
+    @test isapprox(KPSCE3.Y_1(
         0.35931, 0.38601, -0.41703, a, t), 0.360272, atol = 1e-4)
 
     @test isapprox(KPSCE3.Q(a_c), 1.74975, atol = 1e-4)
-    @test isapprox(KPSCE3.M1(1.74975, 1.07049, 0.360272), -1.23918, atol = 1e-4)
-    @test KPSCE3.M2() == 3
-    @test isapprox(KPSCE3.M3(1.74975, 1.07049, -1.23918), -1.926138, atol = 1e-4)
+    @test isapprox(KPSCE3.M_1(1.74975, 1.07049, 0.360272), -1.23918, atol = 1e-4)
+    @test KPSCE3.M_2() == 3
+    @test isapprox(KPSCE3.M_3(1.74975, 1.07049, -1.23918), -1.926138, atol = 1e-4)
     
-     @test isapprox(KPSCE3.h90(x, a, -1.23918, 3, -1.926138), 1.682027, atol = 1e-4)
+     @test isapprox(KPSCE3.h_90(x, a, -1.23918, 3, -1.926138), 1.682027, atol = 1e-4)
             
 end
