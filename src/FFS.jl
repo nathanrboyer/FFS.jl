6e902af3f0a7c0196acc6e6133d1dc581706273c
Maxwell Frame <mframe@aiphip.com>
2026-07-13 10:11:01 -0400

    Setup modules with seperate pluto files

--- src/FFS.jl
+++ src/FFS.jl
@@ -14,10 +14,43 @@ using Interpolations, XLSX, DataFrames, CSV
 
 include("table_import.jl")
 
+#General Fracture Modules
+module Part9
+    include("Part9_eq.plutojl")
+end
+module WFMFSC 
+    include("weighted_f_eq.plutojl")
+end
+
+#Bore Modules
 module RCSCLE3
     include("bore_eq.plutojl")
 end
 
+#Plate Modules
+module KPTC
+    include("plate_thru_wall_k.plutojl")
+end
+module RPTC
+    include("plate_thru_wall_r.plutojl")
+end
+module KPSCE3
+    include("plate_elliptical_k.plutojl")
+end
+module RPSCE
+    include("plate_elliptical_r.plutojl")
+end
+module RPSCL
+    include("plate_infinite_r.plutojl")
+end
+
+#Thread Modules
+module RCSCCE1
+    include("thread_elliptical_r.plutojl")
+end
+module RCSCCL3
+    include("thread_annular_r.plutojl")
+end
 
 # include("Part9_eq.jl")
 # include("bore_eq.jl")
